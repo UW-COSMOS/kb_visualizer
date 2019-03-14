2630179ce4a3bae130e8a494b1ed799d006af4cf
Ian Ross <ian.ackerman.ross@gmail.com>
2019-03-14 09:44:41 -0500

    cleanup

--- server.py
+++ server.py
@@ -14,14 +14,10 @@ cur = conn.cursor(cursor_factory = psycopg2.extras.DictCursor)
 
 cur.execute("SELECT docid FROM docids;")
 docids = cur.fetchall()
-cur.execute("SELECT type FROM types;")
-types = cur.fetchall()
-
-
 
 image_directory = os.getcwd()
-list_of_images = [os.path.basename(i["target_img_path"]) for i in cur.fetchall()]
-static_image_route = '/dummy/'
+
+static_image_route = '/images/'
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -37,7 +33,7 @@ app.layout = html.Div([
     ),
     dcc.Dropdown(
         id='type-dropdown',
-        options=[{'label': i, 'value': i} for i in types],
+#        options=[{'label': i, 'value': i} for i in types],
         placeholder="Select a box type"
     ),
     dcc.Input(
@@ -47,24 +43,10 @@ app.layout = html.Div([
         value=''
     ),
     html.Br(),
-    html.Div(id='content'),
     html.Div(id='my-div'),
-#    html.Div(DataTable(rows=[{}]), style={'display': 'none'})
     ])
 
 app.config['suppress_callback_exceptions']=True
-
-
-
-#@app.callback(
-#    dash.dependencies.Output(component_id='my-div', component_property='children'),
-#    [dash.dependencies.Input('type-dropdown', 'value')])
-#def update_output_div(input_value):
-#    print(input_value)
-#    return 'You\'ve entered "{}"'.format(input_value)
-
-
-
 
 def build_row(row, headers):
     """
@@ -151,26 +133,6 @@ def generate_table(docid, btype, search_term):
 #        print(static_image_route + hit["target_img_path"])
 #        return static_image_route + hit["target_img_path"]
 
-#@app.callback(
-#    dash.dependencies.Output('target_image', 'src'),
-#    [dash.dependencies.Input('docid-dropdown', 'value'),
-#        dash.dependencies.Input('type-dropdown', 'value')])
-#def update_image_src(docid, btype):
-#    print(docid, btype)
-#    docid = docid[0]
-#    btype = btype
-#    # output of this gets fed into the IMG tag above ^
-#    print(cur.mogrify("SELECT * FROM things WHERE target_img_path ~ '%(docid)s.*%(btype)s\d' LIMIT 1", {"docid" : AsIs(docid), "btype" : AsIs(btype)}))
-#    cur.execute("SELECT * FROM things WHERE target_img_path ~ '%(docid)s.*%(btype)s\d' LIMIT 1", {"docid" : AsIs(docid), "btype" : AsIs(btype)})
-#    hit = cur.fetchone()
-#    if hit is None:
-#        print("No results...")
-#        return ''
-#    else:
-#        print(static_image_route + hit["target_img_path"])
-#        return static_image_route + hit["target_img_path"]
-#
-
 @app.callback(
     dash.dependencies.Output('docid-dropdown', 'value'),
     [dash.dependencies.Input('docid-dropdown', 'options')])
@@ -192,37 +154,16 @@ def update_types(docid):
         docid=".*"
     else:
         docid = docid[0]
-    print(cur.mogrify("SELECT DISTINCT substring(target_img_path, '^(?:img/){1}(?:%(docid)s)(?:_input.pdf).*\/(.*[^\d])(:?\d+.png)$') AS type FROM things", {"docid" : AsIs(docid)}))
-    cur.execute("SELECT DISTINCT substring(target_img_path, '^(?:img/){1}(?:%(docid)s)(?:_input.pdf).*\/(.*[^\d])(:?\d+.png)$') AS type FROM things", {"docid" : AsIs(docid)})
+    cur.execute("SELECT DISTINCT substring(target_img_path, '^(?:img/){1}(?:%(docid)s)(?:_input.pdf).*\/(.*[^\d])(:?\d+.png)$') AS type FROM things ORDER BY type ASC", {"docid" : AsIs(docid)})
     tmp = [{'label' : i['type'], 'value' : i['type']} for i in cur.fetchall() if i['type'] is not None]
     return tmp
 
-# Add a static image route that serves images from desktop
-# Be *very* careful here - you don't want to serve arbitrary files
-# from your computer or server
-#@app.server.route('{}/<path:image_path>'.format(static_image_route))
-@app.server.route('/dummy/<path:image_path>'.format(static_image_route))
+@app.server.route('/images/<path:image_path>'.format(static_image_route))
 def serve_image(image_path):
     print("Serving %s" % image_path)
 #    image_name = '{}.png'.format(image_path)
     image_name = '{}'.format(image_path)
-#    if image_name not in list_of_images:
-#        import pdb; pdb.set_trace()
-#        raise Exception('"{}" is excluded from the allowed static files'.format(image_path))
     return flask.send_from_directory(image_directory, image_name)
-
-@app.server.route('/dummy/<path:subpath>')
-def show_subpath_dummy(subpath):
-    # show the subpath after /path/
-    print("Subpath")
-    print(subpath)
-    return 'Subpath %s' % subpath
-@app.server.route('/static/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    print("Subpath")
-    print(subpath)
-    return 'Subpath %s' % subpath
 
 if __name__ == '__main__':
     app.run_server(debug=True, port=8051)
