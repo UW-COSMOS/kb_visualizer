3328661c8d8c8d635e7e4bc999b4976f6b5e69ed
Ian Ross <ian.ackerman.ross@gmail.com>
2019-03-20 10:54:44 -0500

    move table/schema creation out a level (to image-tagger-api)

--- setup.sh
+++ setup.sh
@@ -8,9 +8,7 @@ else
     echo $PG_CONN_STR
 fi
 
-echo "Creating stuff in schema $1"
-
-psql "$PG_CONN_STR" -c "CREATE schema $1;"
+echo "Creating tmp tables and importing data to schema $1"
 
 psql "$PG_CONN_STR" -c "CREATE TABLE IF NOT EXISTS $1.output_tmp (
         document_name text,
@@ -94,111 +92,7 @@ CREATE TABLE IF NOT EXISTS $1.tables_tmp (
         assoc_tesseract text,
         html_file text,
         UNIQUE (target_img_path)
-        );
-CREATE TABLE IF NOT EXISTS $1.output (
-        document_name text,
-        id int, 
-        text text,
-        document_id int,
-        equation_id int,
-        equation_text text,
-        equation_offset text, 
-        sentence_id int,
-        sentence_offset int,
-        sentence_text text,
-        score float,
-        var_top	int,
-        var_bottom int,
-        var_left int,
-        var_right int,
-        var_page int,
-        sent_xpath text,
-        sent_words text[],
-        sent_top text[],
-        sent_table_id int,
-        sent_section_id int,
-        sent_row_start int,
-        sent_row_end int,
-        sent_right int[],
-        sent_position int,
-        sent_pos_tags text[],
-        sent_paragraph_id int,
-        sent_page int[],
-        sent_ner_tags text[],
-        sent_name text,
-        sent_lemmas text[],
-        sent_left int[],
-        sent_html_tag text,
-        sent_html_attrs text[],
-        sent_document_id int,
-        sent_dep_parents text[],
-        sent_dep_labels text[],
-        sent_col_start int,
-        sent_col_end int,
-        sent_char_offsets int[],
-        sent_cell_id int,
-        sent_bottom int[],
-        sent_abs_char_offsets int[],
-        equation_top int,
-        equation_bottom	int,
-        equation_left int,
-        equation_right int,
-        equation_page int,
-        equation_text_duplicate text,
-        symbols text[],
-        phrases text[],
-        phrases_top text[],
-        phrases_bottom text[],
-        phrases_left text[],
-        phrases_right text[],
-        phrases_page text[],
-        sentence_img text,
-        equation_img text,
-        UNIQUE (document_name, id)
-        );
-
-CREATE TABLE IF NOT EXISTS $1.figures (
-        target_img_path text,
-        target_unicode text,
-        target_tesseract text,
-        assoc_img_path text,
-        assoc_unicode text,
-        assoc_tesseract text,
-        html_file text,
-        UNIQUE (target_img_path)
-        );
-
-CREATE TABLE IF NOT EXISTS $1.tables (
-        target_img_path text,
-        target_unicode text,
-        target_tesseract text,
-        assoc_img_path text,
-        assoc_unicode text,
-        assoc_tesseract text,
-        html_file text,
-        UNIQUE (target_img_path)
         );"
-
-psql "$PG_CONN_STR" -c "CREATE VIEW $1.figures_and_tables AS ( SELECT figures.target_img_path,
-        figures.target_unicode,
-        figures.target_tesseract,
-        figures.assoc_img_path,
-        figures.assoc_unicode,
-        figures.assoc_tesseract,
-        figures.html_file
-        FROM $1.figures
-        UNION
-        SELECT tables.target_img_path,
-        tables.target_unicode,
-        tables.target_tesseract,
-        tables.assoc_img_path,
-        tables.assoc_unicode,
-        tables.assoc_tesseract,
-        tables.html_file
-        FROM $1.tables);
-
-CREATE VIEW $1.docids AS ( SELECT DISTINCT substring(figures.target_img_path, '^(?:img/){1}(.*)?_input.*'::text) AS docid
-   FROM $1.figures);"
 
 psql "$PG_CONN_STR" -c "\\copy $1.figures_tmp(
         target_img_path,
@@ -286,4 +180,4 @@ equation_img
 ) FROM '$(pwd)/output/output.csv' DELIMITER ',' CSV HEADER;"
 
 
-psql "$PG_CONN_STR" -c "INSERT INTO $1.output SELECT * FROM $1.output_tmp ON CONFLICT DO NOTHING; DROP TABLE $1.output_tmp;"
+psql "$PG_CONN_STR" -c "INSERT INTO $1.output SELECT * FROM $1.output_tmp ON CONFLICT DO NOTHING; DROP TABLE $1.output_tmp; REFRESH MATERIALIZED VIEW $1.sentence; REFRESH MATERIALIZED VIEW $1.variable; REFRESH MATERIALIZED VIEW $1.phrase;"
